87a93159e82df8e91e5f84d524c84a34c23bd114
hemantsinghrajput <hemantr128@.com>
2025-11-03 21:50:39 +0530

    notification check

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-export const API_URL = 'https://bnsn.in/api/v2'
-//export const API_URL = 'http://192.168.1.11:8000/v2'
+// export const API_URL = 'https://bnsn.in/api/v2'
+export const API_URL = 'http://192.168.31.121:8000/v2'
