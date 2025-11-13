733f96d01d57daa50e321d41d1fce6a4c5f2e879
Jenish <116103065+Jenish00007@users.noreply.github.com>
2025-11-14 00:17:08 +0530

    kkj

--- src/config/api.js
+++ src/config/api.js
@@ -1,2 +1,2 @@
-// export const API_URL = 'https://bnsn.in/api/v2'
-export const API_URL = 'http://192.168.31.121:8000/v2'
+export const API_URL = 'https://bnsn.in/api/v2'
+//export const API_URL = 'http://192.168.1.7:8000/v2'  
